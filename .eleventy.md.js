f723ef46130333025c0f94254c0a7c2afe2e061e
Vadym Veremeichyk <28811614+haiarha@users.noreply.github.com>
2024-03-15 22:58:17 +0000

    Final fixes (#6)
    * Fonts fixes
    
    * Improve about page
    
    * rename project

--- .eleventy.md.js
+++ .eleventy.md.js
@@ -5,15 +5,15 @@ const markdownIt = require("markdown-it");
 // className={
 const headingClassesMap = {
   h1: "mb-8 text-grey6 font-sans-heading text-hL md:text-hXL",
-  h2: "mb-4 mt-[7.5rem] text-grey6 font-sans-heading text-hM md:text-hL uppercase",
-  h3: "mb-4 mt-12 text-grey6 font-sans-heading text-hS md:text-hM",
-  h4: "mb-4 text-grey6 text-hS",
-  h5: "mb-3 text-grey6 text-hS",
-  h6: "mb-2 text-grey6 text-hS",
+  h2: "mb-4 mt-14 md:mt-20 text-grey6 font-sans-heading text-hS md:text-hL font-bold uppercase",
+  h3: "mb-[0.875rem] md:mb-4 mt-8 md:mt-10 text-grey6 font-sans-heading text-hS md:text-hM",
+  // most likely unused
+  h4: "my-3 text-grey6 font-sans-heading text-hS",
+  h5: "my-2 text-grey6 font-sans-heading text-hS",
+  h6: "my-1 text-grey6 font-sans-heading text-hS",
 };
 const maxWidthClass = "max-w-[793px] mx-auto"
 
-// Initialize markdown-it
 let markdownLibrary = markdownIt({
   html: true,
   breaks: true,
@@ -41,7 +41,7 @@ markdownLibrary.renderer.rules.heading_open = function (
 
   return self.renderToken(tokens, idx, options);
 };
-markdownLibrary.renderer.rules.paragraph_open = () => `<p class="mb-4 ${maxWidthClass}">`;
+markdownLibrary.renderer.rules.paragraph_open = () => `<p class="mb-[0.875rem] ${maxWidthClass}">`;
 markdownLibrary.renderer.rules.image_open = () => `<img class="mb-20 w-full">`;
 
 module.exports = markdownLibrary;
---
+++ src/about.md
@@ -0,0 +1,22 @@
+---
+layout: layout-about.njk
+title: About Me
+imgSrc: https://github.com/haiarha/em-per.github.io/assets/28811614/4062ee02-c444-4290-9458-08a2fc877d23
+cvUrl: https://www.ucl.ac.uk/careers/sites/careers/files/your_cv_ucg.pdf
+---
+
+My key value is quality work. In my work I always balance between user needs and business goals.
+
+I always collaborate with developing and business management teams to enhance engagement and work productivity.
+
+I actively use different self-management techniques to stay efficient.
+
+I follow a minimalist lifestyle and apply its principles to my design work.
+
+My key value is quality work. In my work I always balance between user needs and business goals.
+
+I always collaborate with developing and business management teams to enhance engagement and work productivity.
+
+I actively use different self-management techniques to stay efficient.
+
+I follow a minimalist lifestyle and apply its principles to my design work.
